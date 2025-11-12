f155dd9d905c8c92ad6e0fb9e590eb1c302d176b
Bruno Leite <brunoleiteem@gmail.com>
2025-11-12 02:33:03 -0300

    fix

--- js/contact.js
+++ js/contact.js
@@ -1,14 +1,11 @@
 const contactForm = document.querySelector("#contact form");
 
-const scriptUrl =
-  "https://script.google.com/macros/s/AKfycbzJBMGkLXYOhK68Luz7kdKtiICaYSs2BUVbXPnieXPmlI4nu3gqY5mrVmtwuFsL9INj/exec";
-
 contactForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const data = {};
   const inputs = contactForm.querySelectorAll("input");
-  console.log(inputs);
+
   for (const input of inputs) {
     data[input.name] = input.value;
   }
